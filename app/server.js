7ea1c1cbaa0f9ac108440eea21c969fb9e0ff3c8
StevenXPS13 <steven.parrott@gmail.com>
2019-02-09 22:49:56 -0700

    changed path for start

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 var app = express();
 
 
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,7 +17,7 @@ require('./routing/htmlRoutes')(app);
 
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + port);
+    console.log("App listening on PORT " + PORT);
 })
 
 
